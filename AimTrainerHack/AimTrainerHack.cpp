a72ee1df3421e342d6d06827345c34d6730507a3
Hyeongmin Kim <doritos2498@gmail.com>
2024-11-03 23:20:07 +0900

    [feat] Add setTargetColor func
    - To set target color through user's pixel click
    - fix red color condition typo

--- AimTrainerHack/AimTrainerHack.cpp
+++ AimTrainerHack/AimTrainerHack.cpp
@@ -11,6 +11,45 @@ typedef struct {
 
 POINT topLeft, bottomRight, monitorArea;
 int width, height;
+bool wasPressed = false;
+RGBColor targetColor = { 0, 0, 0 }; // 탐지할 색상 초기화
+
+
+// 클릭한 좌표의 RGB 값을 가져오는 함수
+RGBColor getColorAtCursor(POINT pt) {
+    RGBColor color;
+    HDC hdcScreen = GetDC(NULL);
+    COLORREF pixelColor = GetPixel(hdcScreen, pt.x, pt.y);
+    color.red = GetRValue(pixelColor);
+    color.green = GetGValue(pixelColor);
+    color.blue = GetBValue(pixelColor);
+    ReleaseDC(NULL, hdcScreen);
+    return color;
+}
+
+// 사용자 클릭을 통해 탐지할 색상을 설정하는 함수
+void setTargetColor() {
+    printf("탐지할 색상을 설정하려면 원하는 위치를 클릭하세요...\n");
+
+    // 마우스 눌림(press) 대기
+    while (1) {
+        if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {  // 마우스 왼쪽 버튼이 눌림 상태일 때
+            if (!wasPressed) { // 이전에 눌리지 않았을 경우에만 실행
+                POINT pt;
+                GetCursorPos(&pt);
+                targetColor = getColorAtCursor(pt); // 클릭 지점의 색상 설정
+                printf("탐지할 색상 RGB: (%d, %d, %d)\n", targetColor.red, targetColor.green, targetColor.blue);
+                wasPressed = true; // 눌린 상태로 설정
+                Sleep(200); // 클릭 후 짧은 대기
+                break;
+            }
+        }
+        else {
+            wasPressed = false; // 버튼에서 손을 뗀 상태로 초기화
+        }
+        Sleep(50); // CPU 사용량 절약
+    }
+}
 
 void mouseClick() {
     INPUT inputs[2] = { 0 };
@@ -23,7 +62,6 @@ void mouseClick() {
 }
 
 void setMonitorArea() {
-    bool wasPressed = false;
     printf("스페이스바를 눌러 두 지점을 클릭하세요.\n");
     while (!GetAsyncKeyState(VK_SPACE)) {
         Sleep(100);
@@ -33,13 +71,13 @@ void setMonitorArea() {
     while (true) {
         if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
             if (!wasPressed) {
-            GetCursorPos(&topLeft);
-            monitorArea = topLeft;
-            printf("좌상단 지점 클릭: (%d, %d)\n", monitorArea.x, monitorArea.y);
+                GetCursorPos(&topLeft);
+                monitorArea = topLeft;
+                printf("좌상단 지점 클릭: (%d, %d)\n", monitorArea.x, monitorArea.y);
                 wasPressed = true;
                 Sleep(200);
-            break;
-        }
+                break;
+            }
         }
         else {
             wasPressed = false;
@@ -51,12 +89,12 @@ void setMonitorArea() {
     while (true) {
         if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
             if (!wasPressed) {
-            GetCursorPos(&bottomRight);
-            printf("우하단 지점 클릭: (%d, %d)\n", bottomRight.x, bottomRight.y);
+                GetCursorPos(&bottomRight);
+                printf("우하단 지점 클릭: (%d, %d)\n", bottomRight.x, bottomRight.y);
                 wasPressed = true;
                 Sleep(200);
-            break;
-        }
+                break;
+            }
         }
         else {
             wasPressed = false;
@@ -75,7 +113,7 @@ void setMonitorArea() {
 }
 
 
-void captureAndDetectColor(RGBColor targetColor) {
+void captureAndDetectColor() {
     HDC hdcScreen = GetDC(NULL); // 화면의 DC 가져오기
     HDC hdcMem = CreateCompatibleDC(hdcScreen); // 메모리 DC 생성
     HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, width, height);
@@ -104,7 +142,7 @@ void captureAndDetectColor(RGBColor targetColor) {
             BYTE green = pPixels[index + 1];
             BYTE red = pPixels[index + 2];
 
-            if (red >= targetColor.red && green == targetColor.green && blue == targetColor.blue) {
+            if (red == targetColor.red && green == targetColor.green && blue == targetColor.blue) {
                 printf("Detected target color at (%d, %d)\n", x + monitorArea.x, y + monitorArea.y);
                 SetCursorPos(x + monitorArea.x, y + monitorArea.y);
                 mouseClick();
@@ -131,16 +169,16 @@ bool checkForExitKey() {
 }
 
 int main() {
-    RGBColor targetColor = { 255, 87, 34 };
-
     setMonitorArea();
+
+    setTargetColor();
 
     while (1) {
         if (checkForExitKey()) {
             printf("Exiting...\n");
             break;
         }
-        captureAndDetectColor(targetColor);
+        captureAndDetectColor();
         Sleep(100); // 주기적 탐지
     }
     return 0;
